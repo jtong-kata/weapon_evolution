216e6b69a77b02e1df00da187c55cac683991a91
jtong <tj19832@gmail.com>
2014-01-24 01:08:30 +0800

    jtong - refactor, make code less

--- ruby/lib/weapon_evolution/player.rb
+++ ruby/lib/weapon_evolution/player.rb
@@ -1,6 +1,12 @@
 class Player
  attr_accessor :name, :hp, :attack_point
 
+ def initialize args = {}
+   args.each do |key, value|
+     self.instance_variable_set("@#{key}", value) unless value.nil?
+   end
+ end
+
  def beat enemy
    enemy.hp -= self.attack_point
    if enemy.hp > 0
--- ruby/spec/exam1_spec.rb
+++ ruby/spec/exam1_spec.rb
@@ -1,26 +1,15 @@
 require 'weapon_evolution/player'
 
 describe "Player" do
+  let(:zhangsan) { Player.new name: "张三", hp: 10, attack_point: 8 }
+
+  let(:lisi) { Player.new name: "李四", hp: 20, attack_point: 9 }
+
   it "has health point, attack point, name " do
-    player = Player.new
-    player.name = "张三"
-    player.hp = 10
-    player.attack_point = 8
-    expect(player.name).to eq "张三"
+    expect(zhangsan.name).to eq "张三"
   end
 
   it "should beat each other until dead" do
-    zhangsan = Player.new
-    zhangsan.name = "张三"
-    zhangsan.hp = 10
-    zhangsan.attack_point = 8
-
-    lisi = Player.new
-    lisi.name = "李四"
-    lisi.hp = 20
-    lisi.attack_point = 9
-
     lisi.beat(zhangsan).should eq "张三被打败了"
-
   end
 end
