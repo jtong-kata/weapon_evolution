ceff7fb89d80a8e4f157ed84b3740149f12fd093
jtong <tj19832@gmail.com>
2014-01-23 21:16:40 +0800

    jtong -  finished question 1

--- ruby/lib/weapon_evolution/player.rb
+++ ruby/lib/weapon_evolution/player.rb
@@ -1,3 +1,13 @@
 class Player
  attr_accessor :name, :hp, :attack_point
+
+ def beat enemy
+   enemy.hp -= self.attack_point
+   if enemy.hp > 0
+     return enemy.beat self
+   else
+     "#{enemy.name}被打败了"
+   end
+ end
+
 end
--- ruby/spec/exam1_spec.rb
+++ ruby/spec/exam1_spec.rb
@@ -8,4 +8,19 @@ describe "Player" do
     player.attack_point = 8
     expect(player.name).to eq "张三"
   end
+
+  it "should beat each other until dead" do
+    zhangsan = Player.new
+    zhangsan.name = "张三"
+    zhangsan.hp = 10
+    zhangsan.attack_point = 8
+
+    lisi = Player.new
+    lisi.name = "李四"
+    lisi.hp = 20
+    lisi.attack_point = 9
+
+    lisi.beat(zhangsan).should eq "张三被打败了"
+
+  end
 end
