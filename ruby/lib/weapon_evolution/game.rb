c68499f955f660d779f30f0538fcf295f417e569
jtong <tj19832@gmail.com>
2014-02-02 15:03:21 +0800

    jtong - question 4 - use return remove else

--- ruby/lib/weapon_evolution/game.rb
+++ ruby/lib/weapon_evolution/game.rb
@@ -2,10 +2,9 @@ class Game
   def fight_between player_a, player_b
     puts player_a.attack player_b
     if player_b.hp > 0
-      fight_between player_b, player_a
-    else
-      puts "#{player_b.name}被打败了"
+      return fight_between player_b, player_a
     end
 
+    return puts "#{player_b.name}被打败了"
   end
 end
